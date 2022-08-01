d16bf3e5eacac2dc89e7f64fe86e98a717f7bbb8
julieprunaret <j.prunaret@gmail.com>
2022-08-01 16:03:22 +0200

    asserts

--- src/Form/ProductsType.php
+++ src/Form/ProductsType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-
 class ProductsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
