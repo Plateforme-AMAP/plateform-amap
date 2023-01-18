fc912adc4782bd6760d4e110668fbdf79db3e4be
julieprunaret <j.prunaret@gmail.com>
2023-01-18 14:59:16 +0100

    81-conception-du-style: ADD javascript for menu-arbo molecule

--- assets/scripts/menu-arbo.js
+++ assets/scripts/menu-arbo.js
@@ -1,14 +1,25 @@
 export default class MenuToggleDisplay {
     constructor() {
-      this.menu = document.querySelector('.js-menu-element');
       this.menuButton = document.querySelector('.js-menu-button');
+      this.menuZone = document.querySelector('.js-menu-zone');
+      this.menu = document.querySelector('.m-menuArbo');
+      this.icon = document.querySelector('.js-menu-button img');
+
       this.init();
     }
   
     init() {
-        this.menuButton.addEventListener('click', () => {
-            console.log("connecté !");
-        })
+        this.menuButton.addEventListener('click', (e) => {
+            e.target.closest('.m-menuArbo').classList.toggle('-open');
+            if (this.menu.classList.contains('-open')){
+              this.icon.src =  this.icon.src.replace("/icons/tree.svg", "/icons/close.svg"); 
+            } else {
+              this.icon.src =  this.icon.src.replace("/icons/close.svg", "/icons/tree.svg"); 
+            }
+        });
+        this.menuZone.addEventListener('click', (e) => {
+          e.target.closest('.m-menuArbo').classList.toggle('-open');
+        });
     }
   }
 
