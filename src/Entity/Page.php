c63c794740887f3ebfcc048120d14976afac2423
julieprunaret <j.prunaret@gmail.com>
2023-02-17 13:47:46 +0100

    pages-45: add Page home + dynamisation

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -4,8 +4,12 @@ namespace App\Entity;
 
 use App\Repository\PageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
 
 #[ORM\Entity(repositoryClass: PageRepository::class)]
+#[Vich\Uploadable] 
 class Page
 {
     #[ORM\Id]
@@ -22,8 +26,12 @@ class Page
     #[ORM\Column(type: 'string', length: 300, nullable: true)]
     private $description;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $imageName;
+    // NOTE: This is not a mapped field of entity metadata, just a simple property.
+    #[Vich\UploadableField(mapping: 'page_image', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $imageName = null;
 
     #[ORM\Column(type: 'datetime_immutable')]
     private $createdAt;
@@ -73,6 +81,26 @@ class Page
         $this->description = $description;
 
         return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+        
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getImageName(): ?string
--- src/Form/PageType.php
+++ src/Form/PageType.php
@@ -3,22 +3,45 @@
 namespace App\Form;
 
 use App\Entity\Page;
+use App\Entity\Products;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('subtitle')
-            ->add('description')
-            ->add('imageName')
-            ->add('createdAt')
-            ->add('subtitle2')
-            ->add('featuredProduct')
+            ->add('title', TextType::class, [
+                'label' => 'Nom de la page'
+            ])
+            ->add('subtitle', TextType::class, [
+                'label' => 'Sous-titre'
+            ])
+            ->add('subtitle2', TextType::class, [
+                'label' => 'Sous-titre 2 ( optionnel )',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('imageFile', VichImageType::class, [
+                "label" => 'Image principale',
+                "required" => false,
+                "imagine_pattern" => 'min',
+                'allow_delete' => true,
+                'delete_label' => 'Supprimer l\'image',
+            ])
+            ->add('featuredProduct', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Products::class,
+                'choice_label' => 'product_name', //on affiche tout les champs de la classe en option, ce qui est dans "libellé"
+                'placeholder' => 'Choisir un produit phare',
+            ])
         ;
     }
 
